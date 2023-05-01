e539f40bb10d8a0b529b2d204efea1f6958716a2
Michael Adams <michael@21qm.de>
2023-05-01 16:26:51 +0200

    Testcode

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,24 @@
 <script>
 export default {
   name: 'App',
+  async created() {
+    // const request = this.$api.request()
+    // request.host('http://localhost/rest-api')
+    // request.lang()
+    // request.fields()
+    // request.all()
+    // request.limit()
+    // request.page()
+    // request.order()
+    // request.parse('raw', 'router-links', 'nl2br', 'include-label')
+    // const languages = await request.languages()
+    // const node = await request.node()
+    // const children = request.children()
+    // const json = await this.$api.request().fields('files').node('apitest')
+    // json.log()
+    // const image = this.$api.image(json.content.files[0], 400, 400)
+    // console.log(image.src)
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -3,6 +3,6 @@ import App from './App.vue'
 import apiPlugin from './kirbyRestApiVuePlugin'
 
 createApp(App).use(apiPlugin, {
-  host: 'http://localhost:8081',
-  lang: 'de',
+  host: 'http://localhost:8081/rest-api',
+  // lang: 'de',
 }).mount('#app')
